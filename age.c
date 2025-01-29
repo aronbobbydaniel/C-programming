2bd78c2ea204346c5e40b987a3224a4dd578f028
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2025-01-29 16:19:11 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int age;
+  printf("Enter age:");
+  scanf("%d",&age);
+  if (age>=18){
+  printf("You are a adult");
+  }
+  else{
+  printf("Not adult");
+  }
+return 0;
+
+}
