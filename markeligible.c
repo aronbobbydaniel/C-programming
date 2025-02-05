3ce2481899e50414f43f3deeea16cac31a592b49
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2025-02-05 13:46:25 +0530

    Add files via upload

---
+++ markeligible.c
@@ -0,0 +1,14 @@
+#include<stdio,h>
+int main(){
+  int che,phy,math,total,total2;
+  printf("Enter Marks of Chemistry,Physics,Maths respectively:");
+  scanf("%d%d%d",&che,&phy,&math);
+  total=che+phy+math;
+  total2=math+phy;
+  if (maths >=65 && Phy >=55 && Che>=50 && total>=190 ||  total2>=140){
+    printf(" Eligible")
+  }
+  else{
+    printf("Not eligible")
+  }
+}
