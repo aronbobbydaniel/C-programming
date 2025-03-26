2e55f98348e20c2af48c0d9e259ef206e62ba950
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2025-03-26 16:17:07 +0530

    Update macros.c

--- macros.c
+++ macros.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 #define pi 3.14159
 #define circle pi*radius*radius
-#define cylinder pi*radius*radius*height
+#define cylinder 2*pi*radius*(radius+height)
 int main(){
   int radius,height;
   printf("Enter the radius of circle and cylinder and height of cylinder:");
   scanf("%d%d",&radius,&height);
-  printf("Area of the circle:%f",circle);
-  printf("Area of the cylinder:%f",cylinder);
-return 0;
+  printf("\n Area of the circle=%f",circle);
+  printf("\n Area of the cylinder=%f",cylinder);
+return 0; 
 }
-  
