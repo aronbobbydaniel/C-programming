99041abc4ea36b2219d27eedd3e7ac0caf6f2b6a
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2025-04-02 15:46:14 +0530

    Add files via upload

---
+++ employee.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+struct empolyee{
+  char name[20];
+  int ID;
+  float salary;
+};
+int main(){
+  int limit;
+  printf("Enter the Number of Employees for adding details:");
+  scanf("%d",&limit);
+  
+  struct empolyee s[limit];
+  for(int i=0;i<limit;i++){
+    printf("Enter the Name:");
+    scanf("%s",s[i].name);
+    printf("Enter the ID number:");
+    scanf("%d",&s[i].ID);
+    printf("Enter the Salary:");
+    scanf("%f",&s[i].salary);
+    }
+  printf(" \n The Employee Details are:");
+  for(int i=0;i<limit;i++){
+    
+    printf("\n Name is:%s",s[i].name);
+    printf("\n ID Number is:%d",s[i].ID);
+    printf("\n Salary is:%.2f",s[i].salary);
+  }
+return 0;
+}    
