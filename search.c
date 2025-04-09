356e9ca380dcb9d1030f112b6b31c63c3a089e32
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2025-04-09 14:56:40 +0530

    Add files via upload

--- search.c
+++ search.c
@@ -1,12 +1,39 @@
 #include<stdio.h>
+void readFile(FILE*fp){
+  int num;
+  while(fscanf(fp,"%d",&num)==1){
+    printf("%d",num);
+  }
+  fclose(fp);
+}
+
+
+
+
+
 int main(){
-  FILE*fp1,fp2,fp3;
-  fp1=fopen("ddg.txt","r");
+  FILE*fp1,*fp2,*fp3;
+  
+  fp1=fopen("num.txt","r");
+  fp2=fopen("odd.txt","w");
+  fp3=fopen("even.txt","w");
+  
+  
   int num;
   while(fscanf(fp1,"%d",&num)==1){
-    fscanf(fp1,"%d",&num);
-    printf("%d\t",num);
-  }
+    if(num%2==0){
+     fprintf(fp3,"%d",num);
+    }else{
+      fprintf(fp2,"%d",num);
+    }
+   }
   fclose(fp1);
+  fclose(fp2);
+  fclose(fp3);
+  printf("\n The even number file contents:");
+  fp3=fopen("even.txt","r");
+  readFile(fp3);
+  
+  
   return 0;
 } 
